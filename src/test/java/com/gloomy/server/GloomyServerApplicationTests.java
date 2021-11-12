b924c8641065d94d4a1137a310974490bf843ae7
jaesungahn91 <jaesungahn91@gmail.com>
2021-11-12 23:58:31 +0900

    init

---
+++ src/test/java/com/gloomy/server/GloomyServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gloomy.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GloomyServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
