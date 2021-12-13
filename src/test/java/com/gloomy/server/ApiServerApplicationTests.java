84bb0febfc795fbcc6ad0a117a081f9a838fae67
pu1et <pu1etproof@gmail.com>
2021-12-14 05:57:53 +0900

    test : 설정 파일 추가로 인한 Application 테스트 코드 수정

--- src/test/java/com/gloomy/server/ApiServerApplicationTests.java
+++ src/test/java/com/gloomy/server/ApiServerApplicationTests.java
@@ -1,9 +1,16 @@
 package com.gloomy.server;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-@SpringBootTest
+@Slf4j
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(properties = {
+        "spring.config.location=classpath:application.yml,classpath:aws.yml"
+})
 class ApiServerApplicationTests {
 
     @Test
