586f89af9b4bcd9a6527abf9ce6826e29d420dd7
pu1et <pu1etproof@gmail.com>
2021-12-24 17:16:06 +0900

    test : CommentService 댓글 조회 테스트 코드 추가

--- src/test/java/com/gloomy/server/application/comment/CommentServiceTest.java
+++ src/test/java/com/gloomy/server/application/comment/CommentServiceTest.java
@@ -85,9 +85,57 @@ class CommentServiceTest {
         checkCreatedCommentFail(userCommentDTOWithoutUserId);
     }
 
+    @Test
+    void 댓글_조회_비회원_성공() {
+        Comment createdComment = commentService.createComment(testCommentDTO.makeNonUserCommentDTO());
+
+        Comment foundComment = commentService.findComment(createdComment.getId());
+
+        assertEquals(foundComment, createdComment);
+    }
+
+    @Test
+    void 댓글_조회_비회원_실패() {
+        Comment createdComment = commentService.createComment(testCommentDTO.makeNonUserCommentDTO());
+
+        commentService.deleteAll();
+
+        checkFoundCommentFail(createdComment.getId(), "[CommentService] 해당 댓글 ID가 존재하지 않습니다.");
+    }
+
+    @Test
+    void 댓글_조회_회원_성공() {
+        Comment createdComment = commentService.createComment(testCommentDTO.makeUserCommentDTO());
+
+        Comment foundComment = commentService.findComment(createdComment.getId());
+
+        assertEquals(foundComment, createdComment);
+    }
+
+    @Test
+    void 댓글_조회_회원_실패() {
+        Comment createdComment = commentService.createComment(testCommentDTO.makeUserCommentDTO());
+
+        commentService.deleteAll();
+
+        checkFoundCommentFail(createdComment.getId(), "[CommentService] 해당 댓글 ID가 존재하지 않습니다.");
+    }
+
+    @Test
+    void 댓글_조회_공통_실패() {
+        checkFoundCommentFail(0L, "[CommentService] 해당 댓글 ID가 유효하지 않습니다.");
+        checkFoundCommentFail(null, "[CommentService] 해당 댓글 ID가 유효하지 않습니다.");
+    }
+
     private void checkCreatedCommentFail(CommentDTO.Request commentDTO) {
         assertThrows(IllegalArgumentException.class, () -> {
             commentService.createComment(commentDTO);
         }, "[CommentService] 비회원 댓글 등록 요청 메시지가 잘못되었습니다.");
+    }
+
+    private void checkFoundCommentFail(Long commentId, String errorMessage) {
+        assertThrows(IllegalArgumentException.class, () -> {
+            commentService.findComment(commentId);
+        }, errorMessage);
     }
 }
