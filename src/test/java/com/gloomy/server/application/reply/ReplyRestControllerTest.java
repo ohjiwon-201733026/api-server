8bfab4c92b226aa655b696a133b88324cf3e34b5
pu1et <pu1etproof@gmail.com>
2022-01-06 20:20:36 +0900

    test : ReplyController 댓글의 대댓글 전체 조회 테스트 추가

--- src/test/java/com/gloomy/server/application/reply/ReplyRestControllerTest.java
+++ src/test/java/com/gloomy/server/application/reply/ReplyRestControllerTest.java
@@ -2,7 +2,6 @@ package com.gloomy.server.application.reply;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.gloomy.server.application.AbstractControllerTest;
-import com.gloomy.server.application.comment.CommentDTO;
 import com.gloomy.server.application.comment.CommentService;
 import com.gloomy.server.application.comment.TestCommentDTO;
 import com.gloomy.server.application.feed.FeedService;
@@ -120,6 +119,43 @@ public class ReplyRestControllerTest extends AbstractControllerTest {
                                 fieldWithPath("commentId").type(JsonFieldType.NUMBER).description("댓글 ID"),
                                 fieldWithPath("userId").type(JsonFieldType.NUMBER).description("회원 ID").optional(),
                                 fieldWithPath("password").type(JsonFieldType.STRING).description("비밀번호").optional()
+                        )
+                ));
+    }
+
+    @DisplayName("댓글_대댓글_전체_조회")
+    @Test
+    void getCommentAllReplies() throws Exception {
+        ReplyDTO.Request request = testReplyDTO.makeNonUserReplyDTO();
+
+        replyService.createReply(request);
+        replyService.createReply(request);
+
+        this.mockMvc.perform(get("/reply/comment/{commentId}", request.getCommentId()))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andDo(document.document(
+                        pathParameters(
+                                parameterWithName("commentId").description("조회할 대댓글의 댓글 ID")),
+                        responseFields(
+                                fieldWithPath("content[].id").type(JsonFieldType.NUMBER).description("대댓글 ID"),
+                                fieldWithPath("content[].content").type(JsonFieldType.STRING).description("대댓글 내용"),
+                                fieldWithPath("content[].commentId").type(JsonFieldType.NUMBER).description("댓글 ID"),
+                                fieldWithPath("content[].userId").type(JsonFieldType.NUMBER).description("회원 ID").optional(),
+                                fieldWithPath("content[].password").type(JsonFieldType.STRING).description("비밀번호").optional(),
+
+                                fieldWithPath("pageable").type(JsonFieldType.STRING).description("pageable 정보"),
+                                fieldWithPath("totalPages").type(JsonFieldType.NUMBER).description("전체 페이지 수"),
+                                fieldWithPath("totalElements").type(JsonFieldType.NUMBER).description("전체 페이지 내 요소의 수"),
+                                fieldWithPath("last").type(JsonFieldType.BOOLEAN).description("마지막 페이지 여부"),
+                                fieldWithPath("numberOfElements").type(JsonFieldType.NUMBER).description("현재 페이지 내 요소의 수"),
+                                fieldWithPath("first").type(JsonFieldType.BOOLEAN).description("첫 페이지 여부"),
+                                fieldWithPath("size").type(JsonFieldType.NUMBER).description("페이지 당 출력 갯수"),
+                                fieldWithPath("sort.sorted").type(JsonFieldType.BOOLEAN).description("정렬 여부"),
+                                fieldWithPath("sort.unsorted").type(JsonFieldType.BOOLEAN).description("비정렬 여부"),
+                                fieldWithPath("sort.empty").type(JsonFieldType.BOOLEAN).description("정렬 비어있는지 여부"),
+                                fieldWithPath("number").type(JsonFieldType.NUMBER).description("현재 페이지 인덱스"),
+                                fieldWithPath("empty").type(JsonFieldType.BOOLEAN).description("비어있는지 여부")
                         )
                 ));
     }
