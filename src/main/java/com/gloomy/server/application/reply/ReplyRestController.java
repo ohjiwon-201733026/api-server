686e49bfe754b5b4d4d3f266e4e687e383cacec0
pu1et <pu1etproof@gmail.com>
2022-01-06 20:24:20 +0900

    feat : ReplyController 댓글의 대댓글 전체 조회 기능 추가

--- src/main/java/com/gloomy/server/application/reply/ReplyRestController.java
+++ src/main/java/com/gloomy/server/application/reply/ReplyRestController.java
@@ -1,6 +1,10 @@
 package com.gloomy.server.application.reply;
 
 import com.gloomy.server.domain.reply.Reply;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
@@ -26,6 +30,24 @@ public class ReplyRestController {
         } catch (IllegalArgumentException e) {
             return new ResponseEntity<>(makeErrorMessage(e.getMessage(), replyDTO.toString()), HttpStatus.BAD_REQUEST);
         }
+    }
+
+    @GetMapping("/comment/{commentId}")
+    public Object getCommentAllActiveReplies(@PageableDefault(size = 10) Pageable pageable, @PathVariable Long commentId) {
+        try {
+            Page<Reply> commentAllReplies = replyService.getCommentAllActiveReplies(pageable, commentId);
+            return new ResponseEntity<>(makeResult(commentAllReplies), HttpStatus.OK);
+        } catch (IllegalArgumentException e) {
+            return new ResponseEntity<>(makeErrorMessage(e.getMessage(), null), HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    private Page<ReplyDTO.Response> makeResult(Page<Reply> allReplies) {
+        List<ReplyDTO.Response> result = new ArrayList<>();
+        for (Reply reply : allReplies.getContent()) {
+            result.add(makeReplyDTOResponse(reply));
+        }
+        return new PageImpl<>(result);
     }
 
     private ReplyDTO.Response makeReplyDTOResponse(Reply reply) {
