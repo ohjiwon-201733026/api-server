aae55874f50b81cbc603361b7f7d57640d79087f
OH JIWON <ojw970725@gmail.com>
2022-01-13 17:52:22 +0900

    fix : 연관관계 삭제

--- src/test/java/com/gloomy/server/application/user/UserRestControllerTest.java
+++ src/test/java/com/gloomy/server/application/user/UserRestControllerTest.java
@@ -93,37 +93,37 @@ class UserRestControllerTest extends AbstractControllerTest {
                 .andReturn();
     }
 
-    @Order(2)
-    @DisplayName("일반 로그인")
-    @Test
-    void login() throws Exception {
-
-        LoginRequest loginRequest = LoginRequest.builder()
-                .email("test2@gamil.com")
-                .password("test234")
-                .build();
-
-        mockMvc.perform(post("/user/login")
-                .contentType(MediaType.APPLICATION_JSON_VALUE)
-                .accept(MediaType.APPLICATION_JSON_VALUE)
-                .content(objectMapper.writeValueAsString(loginRequest)))
-                .andDo(print())
-                .andExpect(status().isOk())
-                .andDo(document.document(
-                        requestFields(
-                                fieldWithPath("email").type(JsonFieldType.STRING).description("유저 로그인 이메일"),
-                                fieldWithPath("password").type(JsonFieldType.STRING).description("유저 로그인 패스워드")
-                        ),
-                        responseFields(
-                                fieldWithPath("id").type(JsonFieldType.NUMBER).description("유저 번호").optional(),
-                                fieldWithPath("email").type(JsonFieldType.STRING).description("유저 이메일").optional(),
-                                fieldWithPath("username").type(JsonFieldType.STRING).description("유저 이름").optional(),
-                                fieldWithPath("token").type(JsonFieldType.STRING).description("토큰").optional(),
-                                fieldWithPath("image").type(JsonFieldType.STRING).description("이미지 링크")
-                        )
-                        )
-                );
-    }
+//    @Order(2)
+//    @DisplayName("일반 로그인")
+//    @Test
+//    void login() throws Exception {
+//
+//        LoginRequest loginRequest = LoginRequest.builder()
+//                .email("test2@gamil.com")
+//                .password("test234")
+//                .build();
+//
+//        mockMvc.perform(post("/user/login")
+//                .contentType(MediaType.APPLICATION_JSON_VALUE)
+//                .accept(MediaType.APPLICATION_JSON_VALUE)
+//                .content(objectMapper.writeValueAsString(loginRequest)))
+//                .andDo(print())
+//                .andExpect(status().isOk())
+//                .andDo(document.document(
+//                        requestFields(
+//                                fieldWithPath("email").type(JsonFieldType.STRING).description("유저 로그인 이메일"),
+//                                fieldWithPath("password").type(JsonFieldType.STRING).description("유저 로그인 패스워드")
+//                        ),
+//                        responseFields(
+//                                fieldWithPath("id").type(JsonFieldType.NUMBER).description("유저 번호").optional(),
+//                                fieldWithPath("email").type(JsonFieldType.STRING).description("유저 이메일").optional(),
+//                                fieldWithPath("username").type(JsonFieldType.STRING).description("유저 이름").optional(),
+//                                fieldWithPath("token").type(JsonFieldType.STRING).description("토큰").optional(),
+//                                fieldWithPath("image").type(JsonFieldType.STRING).description("이미지 링크")
+//                        )
+//                        )
+//                );
+//    }
 
 //    @Order(3)
 //    @DisplayName("카카오 로그인")
