04cfc7727d67d12b96bdde5702f1313214fcd1de
Jiwon An <pu1etproof@gmail.com>
2022-01-24 10:34:52 +0900

    test : FeedController 카테고리 리스트 조회 테스트 추가

--- src/test/java/com/gloomy/server/application/feed/FeedRestControllerTest.java
+++ src/test/java/com/gloomy/server/application/feed/FeedRestControllerTest.java
@@ -359,4 +359,25 @@ class FeedRestControllerTest extends AbstractControllerTest {
                         )
                 ));
     }
+
+    @DisplayName("카테고리 리스트 조회")
+    @Test
+    void getFeedCategories() throws Exception {
+        List<CategoryValue> allCategories = Category.getAllCategories();
+
+        this.mockMvc.perform(get("/feed/category")
+                        .accept(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andDo(document.document(
+                        responseFields(
+                                fieldWithPath("code").type(JsonFieldType.NUMBER).description("응답 상태 코드"),
+                                fieldWithPath("message").type(JsonFieldType.STRING).description("응답 메시지"),
+                                fieldWithPath("result[]").type(JsonFieldType.ARRAY).description("카테고리 리스트"),
+                                fieldWithPath("result[].code").type(JsonFieldType.STRING).description("카테고리 코드"),
+                                fieldWithPath("result[].title").type(JsonFieldType.STRING).description("카테고리 제목"),
+                                fieldWithPath("responseTime").type(JsonFieldType.STRING).description("응답 시간")
+                        )
+                ));
+    }
 }
