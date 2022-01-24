01bf92264bbbc69dc517fdf0dc4627ebd7c84023
Jiwon An <pu1etproof@gmail.com>
2022-01-24 10:35:38 +0900

    feat : FeedController 카테고리 리스트 조회 추가

--- src/main/java/com/gloomy/server/domain/common/Status.java
+++ src/main/java/com/gloomy/server/domain/common/Status.java
@@ -4,14 +4,12 @@ import lombok.Getter;
 
 @Getter
 public enum Status {
-    ACTIVE(1, "활성"),
-    INACTIVE(2, "비활성");
+    ACTIVE("활성"),
+    INACTIVE("비활성");
 
-    private Integer statusCode;
     private String statusName;
 
-    Status(Integer statusCode, String statusName) {
-        this.statusCode = statusCode;
+    Status(String statusName) {
         this.statusName = statusName;
     }
 }
