8cabc5149f9b2c62b433081c70c22fec6f89a8bd
pu1et <pu1etproof@gmail.com>
2022-02-01 01:43:53 +0900

    test : ImageController 피드 이미지 생성 테스트 추가

--- src/test/java/com/gloomy/server/application/image/TestImage.java
+++ src/test/java/com/gloomy/server/application/image/TestImage.java
@@ -3,6 +3,8 @@ package com.gloomy.server.application.image;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
@@ -36,7 +38,6 @@ public class TestImage {
             log.info("[FeedServiceTest] 테스트 파일 이미지 변환 실패했습니다.");
             e.printStackTrace();
         }
-
         for (int i = 0; i < imageNum; i++) {
             images.add(image);
         }
@@ -54,15 +55,13 @@ public class TestImage {
         }
     }
 
-    public static MockMultipartFile convertOne(MultipartFile image) {
-
+    public static MultiValueMap<String, String> convert(Long feedId) {
         try {
-            return new MockMultipartFile("image",
-                    image.getOriginalFilename(),
-                    MediaType.MULTIPART_FORM_DATA_VALUE,
-                    image.getInputStream());
-        } catch (IOException e) {
-            throw new IllegalArgumentException("[TestImage] 이미지 파일 변환 중 오류가 발생했습니다.");
+            MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
+            params.add("feedId", feedId.toString());
+            return params;
+        } catch (Exception e) {
+            throw new IllegalArgumentException("[TestImage] 변환 중 오류가 발생했습니다.");
         }
     }
 }
