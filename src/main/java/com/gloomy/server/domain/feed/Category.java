720e90eb7c532c56cef9d1829656dda3af092f80
pu1et <pu1etproof@gmail.com>
2022-02-16 06:31:17 +0900

    feat : 피드 카테고리 추가
    - COMPANY, EMPLOY, STUDY, HETEROLOVE, HOMOLOVE, UNDEFINED

--- src/main/java/com/gloomy/server/domain/feed/Category.java
+++ src/main/java/com/gloomy/server/domain/feed/Category.java
@@ -4,7 +4,16 @@ import com.fasterxml.jackson.annotation.JsonCreator;
 import com.gloomy.server.domain.common.EnumModel;
 
 public enum Category implements EnumModel {
-    ALL("모든 고민"), CHAT("잡담"), FAMILY("가족"), FRIEND("친구");
+    ALL("모든 고민"),
+    CHAT("잡담"),
+    FAMILY("가족"),
+    FRIEND("친구"),
+    COMPANY("직장/이직"),
+    EMPLOY("취업"),
+    STUDY("학업/시험"),
+    HETEROLOVE("연애(이성애)"),
+    HOMOLOVE("연애(동성애)"),
+    UNDEFINED("미정");
 
     private String title;
 
