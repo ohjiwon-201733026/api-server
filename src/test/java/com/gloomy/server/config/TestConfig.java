782143a5484da6eb67db8da9b6d0c0ef961960b4
OH JIWON <ojw970725@gmail.com>
2022-02-25 22:29:08 +0900

    fix : Logout table 삭제

--- src/test/java/com/gloomy/server/config/TestConfig.java
+++ src/test/java/com/gloomy/server/config/TestConfig.java
@@ -1,21 +1,46 @@
 package com.gloomy.server.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.gloomy.server.application.jwt.JwtServiceTest;
 import com.gloomy.server.domain.user.UserService;
+import com.gloomy.server.infrastructure.jwt.ExpiredTimeTokenHmacSHA256JWTService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
 
+import java.nio.charset.StandardCharsets;
+
 @TestConfiguration
 public class TestConfig {
 
-    @Autowired
-    private UserService userService;
+    private static final byte[] SECRET = "SOME_SIGNATURE_SECRET".getBytes(StandardCharsets.UTF_8);
+    private static final int JWT_ACCESS_TOKEN_EXPIRE_TIME = 21599;
+    private static final int JWT_REFRESH_TOKEN_EXPIRE_TIME=5183999;
 
-    @Bean
-    public ApplicationRunner applicationRunner() {
-        return args -> {
-        };
-    }
+//    @Autowired
+//    private UserService userService;
+
+//    @Bean
+//    public ExpiredTimeTokenHmacSHA256JWTService expiredTimeTokenHmacSHA256JWTService(ObjectMapper objectMapper) {
+//        System.out.println("TestConfig.expiredTimeTokenHmacSHA256JWTService");
+//        return new ExpiredTimeTokenHmacSHA256JWTService(SECRET, JWT_ACCESS_TOKEN_EXPIRE_TIME,JWT_REFRESH_TOKEN_EXPIRE_TIME, objectMapper);
+//    }
+
+//    @Bean
+//    public ObjectMapper objectMapper(){
+//        return new ObjectMapper();
+//    }
+
+//    @Bean
+//    public JwtServiceTest jwtServiceTest()
+
+
+//    @Bean
+//    public ApplicationRunner applicationRunner() {
+//        return args -> {
+//        };
+//    }
 
 }
